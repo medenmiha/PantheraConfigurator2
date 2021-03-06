7dd31c8f44f2e8f784dc11ff30268565fe0fca20
Miha Meden <meden.miha@gmail.com>
2021-03-06 18:17:26 +0100

    1

--- logic.js
+++ logic.js
@@ -217,7 +217,7 @@ document.getElementById("slikaModra").onclick = function() {
 
 document.getElementById("slikaRdeca").onclick = function() {
   //preklop v rdeco
-  partsArray.getObjectByName("Plane").visible = false;
+  partsArray.remove(getObjectByName("Plane"));
   //partsArray[34].visible = true;
   //partsArray[36].visible = true;  //prednjiSedezDesniPanthera2
   //partsArray[35].visible = true;  //prednjiSedezLeviPanthera2 problem
